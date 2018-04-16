b3fa175fe4659810ed82aae37577f4de79274301
Quang Tran <quang.tran@numberfour.eu>
2018-04-16 09:46:08 +0200

    Update generated shipped code

--- publish-shipped-code-npm.sh
+++ publish-shipped-code-npm.sh
@@ -1,38 +1,12 @@
-echo "Start publishing shipped code to npm registry"
-cd `dirname $0`
-cd `pwd -P`
-
-# At this point, we are in the root folder of n4js repo, i.e. n4js
-
-# Install n4js-prepare-npm npm
-npm install n4js-prepare-npm
+#!/bin/bash
 
 # Navigate to shipped-code folder
-cd plugins/org.eclipse.n4js.external.libraries/shipped-code
+cd `dirname $0`/plugins/org.eclipse.n4js.external.libraries/shipped-code
 
-# Testing purpose: publish to test NEXUS npm registry
-NPM_REGISTRY="http://webclients3-nexus.service.cd-dev.consul/repository/npm-internal/"
+rm -rf node_modules
 
+# Install dependencies, e.g. n4js-prepare-npm
+yarn install
 
-# Use n4js-npm-prepare to prepare package.json (e.g. dependencies, licenses) of npms to be published
-n4js-prepare-npm -globProjects **/manifest.n4mf \
-	--verbose	\
-	--npmrcAuthRegistry "${NPM_REGISTRY}" \
-	--npmrcAuthToken "${NPM_TOKEN}"
-
-# Publish all projects in shipped-code folder to npm registry
-#	--skip-git			Don't run any git commands
-#	--registry			The npm registry to publish to
-#	--force-publish=*	For publish for all packages (skips git diff check for changed packages)	
-#	--exact				Specify updated dependencis in updated packages exactly
-#	--canary			Creates canary version, e.g. 1.0.0 -> 1.1.0-alpha.e1sa2334
-# 	--yes				Skip all confirmation promps
-lerna publish \
-	--loglevel silly \	
-	--skip-git \ 
-	--registry="${NPM_REGISTRY}"
-	--force-publish=*
-	--exact
-	--canary
-	--yes
-echo "End of publishing shipped code to npm registry"
+# Run npm task script 'publish-canary'
+yarn run publish-canary
