ec042efc8fd793fd912d0cebae747696d682bafa
Quang Tran <quang.tran@numberfour.eu>
2018-05-04 16:16:33 +0200

    Adjust script

--- n4js-libs/npm-publish-canary.sh
+++ n4js-libs/npm-publish-canary.sh
@@ -2,8 +2,10 @@
 cd `dirname $0`
 cd `pwd -P`
 
+echo "We are currently in $PWD"
+
 # Prepare package.json, e.g. dependencies etc. from manifest before publishing
-n4js-prepare-npm --globProjects "+(lang|runtime|mangelhaft)/*/manifest.n4mf"  --verbose --npmrcAuthRegistry "${NPM_REGISTRY}" --npmrcAuthToken "${NPM_TOKEN}"
+n4js-prepare-npm --verbose --npmrcAuthRegistry "${NPM_REGISTRY}" --npmrcAuthToken "${NPM_TOKEN}"
 
 # Publish using canary version
-lerna publish --loglevel silly --skip-git --registry="${NPM_REGISTRY}" --force-publish --exact --canary --yes --sort 
+lerna publish --loglevel silly --skip-git --registry="${NPM_REGISTRY}" --force-publish --exact --canary --yes --sort
