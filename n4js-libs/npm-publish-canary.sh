0d199e4929218514ae9e72c9bbf63a07fe3f7f97
Quang Tran <quang.tran@numberfour.eu>
2018-05-14 15:09:22 +0200

    Start verdaccio before publishing

--- n4js-libs/npm-publish-canary.sh
+++ n4js-libs/npm-publish-canary.sh
@@ -9,5 +9,8 @@ echo "We are currently in $PWD"
 # Login as a test user
 npm-cli-login -u testuser -p testpass -e test@pass.com -r $NPM_REGISTRY
 
+# Start Verdaccio
+verdaccio -l localhost:4873 &
+
 # Publish using canary version
 lerna publish --loglevel silly --skip-git --registry="${NPM_REGISTRY}" --force-publish --exact --canary --yes --sort
