27a9684d588232d0b3e87775dbe62b5f2570587e
Quang Tran <quang.tran@numberfour.eu>
2018-05-14 15:28:03 +0200

    Fix order

--- n4js-libs/npm-publish-canary.sh
+++ n4js-libs/npm-publish-canary.sh
@@ -6,11 +6,11 @@ NPM_REGISTRY="http://localhost:4873"
 
 echo "We are currently in $PWD"
 
-# Login as a test user
-npm-cli-login -u testuser -p testpass -e test@pass.com -r $NPM_REGISTRY
-
 # Start Verdaccio
 verdaccio -l localhost:4873 &
+
+# Login as a test user
+npm-cli-login -u testuser -p testpass -e test@pass.com -r $NPM_REGISTRY
 
 # Publish using canary version
 lerna publish --loglevel silly --skip-git --registry="${NPM_REGISTRY}" --force-publish --exact --canary --yes --sort
