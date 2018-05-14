1bd30b0221405334ecc469b71782a91f712d911b
Quang Tran <quang.tran@numberfour.eu>
2018-05-14 15:52:38 +0200

    Redirect output of verdaccio

--- n4js-libs/npm-publish-canary.sh
+++ n4js-libs/npm-publish-canary.sh
@@ -7,7 +7,7 @@ NPM_REGISTRY="http://localhost:4873"
 echo "We are currently in $PWD"
 
 # Start Verdaccio
-verdaccio -l localhost:4873 &
+verdaccio -l localhost:4873 &>/dev/null &
 
 # Login as a test user
 npm-cli-login -u testuser -p testpass -e test@pass.com -r $NPM_REGISTRY
